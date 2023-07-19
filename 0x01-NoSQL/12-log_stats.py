cfacd892d667ebc286a50d73d3c3763f46544ee6
Ezekiel nwuguru <eazy4life2017@gmail.com>
2023-07-19 20:20:15 +0100

    Update 12-log_stats.py

--- 0x01-NoSQL/12-log_stats.py
+++ 0x01-NoSQL/12-log_stats.py
@@ -5,29 +5,28 @@
 
 from pymongo import MongoClient
 
+# Connect to the MongoDB server
+client = MongoClient('mongodb://localhost:27017')
 
-def log_stats():
-    """ log_stats.
-    """
-    client = MongoClient('mongodb://127.0.0.1:27017')
-    logs_collection = client.logs.nginx
-    total = logs_collection.count_documents({})
-    get = logs_collection.count_documents({"method": "GET"})
-    post = logs_collection.count_documents({"method": "POST"})
-    put = logs_collection.count_documents({"method": "PUT"})
-    patch = logs_collection.count_documents({"method": "PATCH"})
-    delete = logs_collection.count_documents({"method": "DELETE"})
-    path = logs_collection.count_documents(
-        {"method": "GET", "path": "/status"})
-    print(f"{total} logs")
-    print("Methods:")
-    print(f"\tmethod GET: {get}")
-    print(f"\tmethod POST: {post}")
-    print(f"\tmethod PUT: {put}")
-    print(f"\tmethod PATCH: {patch}")
-    print(f"\tmethod DELETE: {delete}")
-    print(f"{path} status check")
+# Access the "logs" database and "nginx" collection
+db = client['logs']
+collection = db['nginx']
 
+# Get the total number of documents in the collection
+total_logs = collection.count_documents({})
 
-if __name__ == "__main__":
-    log_stats()
+print(f"first line: {total_logs} logs where {total_logs} is the number of documents in this collection")
+
+# Define the list of HTTP methods to check
+http_methods = ["GET", "POST", "PUT", "PATCH", "DELETE"]
+
+# Display the count of each HTTP method in the collection
+print("second line: Methods:")
+for method in http_methods:
+    count = collection.count_documents({"method": method})
+    print(f"\t{method}: {count}")
+
+# Display the count of documents with method=GET and path=/status
+count_status_path = collection.count_documents({"method": "GET", "path": "/status"})
+print(f"one line with the number of documents with:\nmethod=GET\npath=/status: {count_status_path}")
+
